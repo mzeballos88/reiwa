59db294469a1052dc8ed2c3868cc482141dd7b10
mel <zeballos.me@gmail.com>
2021-08-22 15:08:11 -0500

    se agregró la carpeta components con el NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function NavBar() {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">Reiwa Izakaya</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#">Menú</Nav.Link>
+                    <Nav.Link href="#">Nosotros</Nav.Link>
+                    <NavDropdown title="Carta" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Entradas</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Platos de fondo</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Bebidas</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Ver todo</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="#">Contacto</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+        </div>
+    )
+}
