28055dd08b68c0d46d2543dc498fd4f4f561c276
mel <zeballos.me@gmail.com>
2021-08-29 17:45:16 -0500

    se agregó item count

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/Container/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
+  const onAdd = (count) =>{
+    console.log(`La cantidad es: ${count}`)
+    alert(`La cantidad es: ${count}`)
+  }
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer />
+      <ItemCount initial={1} stock={12} onAdd={onAdd} />
+    
     </div>
   );
 }
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
+export default function ItemCount({initial, stock, onAdd}) {
+    const [count, setCount] = useState(initial);
+    const sumaClick = () => {
+      if(count < stock) setCount(count + 1);
+    
+    }
+
+  const restaClick = () => {
+    if(count > initial) setCount(count - 1);
+    }
+
+  const handlerOnAdd=()=> {
+    onAdd(count)
+    setCount(initial)
+   }
+
+  
+    return (
+        <div>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="https://bit.ly/3DvaDmk" />
+            <Card.Body>
+              <Card.Title>Chasu Tamago <br/> チャーシュー玉子</Card.Title>
+              <Card.Text>
+              Finas láminas de cerdo flameadas, acompañadas con huevito ajitsuke.
+              </Card.Text>
+              <label>Cantidad</label> <br />
+             <button className="btn" onClick={sumaClick}>+</button>
+             <h3>{count}</h3>
+             <button className="btn" onClick={restaClick}>-</button><br />
+             <button className="btn btn-dark" onClick={handlerOnAdd}>Agregar</button>
+            </Card.Body>
+        </Card>
+        </div>
+    )
+}
